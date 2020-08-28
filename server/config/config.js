7a901f423bd4c6762f79cfadb4429225525a51cc
Isaac <isaacacejo@gmail.com>
2020-08-28 11:14:42 +0200

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -7,3 +7,30 @@
 //==============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+
+//==============================
+// Entorno ( Produccion )
+//==============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//==============================
+// Base de Datos
+//==============================
+
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+
+    urlDB = 'mongodb+srv://strider:KsUbBOvjYzDSt2gW@cluster0.ul9ez.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
+
+
--- server/server.js
+++ server/server.js
@@ -1,53 +1,38 @@
 
 require('./config/config');
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express()
+
 const bodyParser = require('body-parser')
+
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
+
+app.use(require('./routes/usuario'));
  
-app.get('/usuario', function (req, res) {
-  res.json('Get Usuario')
-});
 
-app.post('/usuario', function (req, res) {
+mongoose.connect(process.env.URLDB,
+            {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+            (err, res) =>{
+    
 
-    let body = req.body;
+    if (err) throw err;
 
-    if (body.nombre === undefined){
-
-        res.status(400).json({
-            ok: false,
-            mensaje: ' El nombre es necesario.'
-        });
-
-    }else{
-        res.json({
-            persona: body
-        });
-    }
+    console.log('Base de datos ONLINE');
 
 });
 
-app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-
-        id
-
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('Delete Usuario')
-});
  
 app.listen(process.env.PORT, () => {
 
     console.log('Escuchando puerto , 3000');
 
 })
+
